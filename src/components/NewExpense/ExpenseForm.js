70e895055cbbb1aa93c71ffe2978d5a576a08543
leonoronhas <leosud099@gmail.com>
2021-04-15 15:29:22 -0600

    New Expense Button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -87,6 +87,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onStopShowExpenseButton}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [showNewExpenseButton, setNewExpenseButton] = useState(true);
+
   const saveExpenseHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,13 +14,31 @@ const NewExpense = (props) => {
 
     // Communication from children to Parent 2nd level
     props.onAddExpense(expenseData);
+    setNewExpenseButton(!showNewExpenseButton);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
-    </div>
-  );
+  const expenseFormHandler = (event) => {
+    setNewExpenseButton(!showNewExpenseButton);
+  };
+
+  if (showNewExpenseButton) {
+    return (
+      <div className="new-expense">
+        <button type="button" onClick={expenseFormHandler}>
+          Add New Expense
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="new-expense">
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseHandler}
+          onStopShowExpenseButton={expenseFormHandler}
+        />
+      </div>
+    );
+  }
 };
 
 export default NewExpense;
